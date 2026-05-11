2f46ac13cb086d120f7a2bd20e3582f06827b208
Yugesh Kanna <yugeshkanna@example.com>
2026-05-11 12:06:08 +0530

    Variable

---
+++ variable.java
@@ -0,0 +1,12 @@
+public class variable {
+    
+    public static void main(String[] args) {
+
+        int x = 4;
+        int $abc = 5;
+        int totalMarks=100;
+        System.out.println(x);
+        System.out.println($abc);
+        System.out.println(totalMarks);
+    }
+}
