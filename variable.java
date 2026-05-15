6d6032aab090f43e07556936acc452d5debc6be2
Yugesh Kanna <yugeshkanna@example.com>
2026-05-15 15:08:19 +0530

    odd/even

--- variable.java
+++ variable.java
@@ -2,9 +2,10 @@ public class variable {
     
     public static void main(String[] args) {
 
-        int x = 4;
-        int $abc = 5;
-        int totalMarks=100;
+        int x = 4;//normal
+        int $abc = 5;//using special character
+        int totalMarks=100;//using camelcase
+        
         System.out.println(x);
         System.out.println($abc);
         System.out.println(totalMarks);
