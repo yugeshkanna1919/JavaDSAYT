e4410029a4afa3fa1a24db584d4db832124a2e3a
Yugesh Kanna <yugeshkanna@example.com>
2026-05-15 16:27:27 +0530

    odd/even

---
+++ evenodd.java
@@ -0,0 +1,10 @@
+public class evenodd {
+    public static void main(String Args[]){
+        int a = 7;
+        if(a%2==0){
+            System.out.printf("This a in even number %d",a);
+        }else{
+            System.out.printf("These is an odd number %d",a);
+        }
+    }
+}
--- main.java
+++ main.java
@@ -1,6 +1,6 @@
 class main
 {
-    public static void main(String[] args){
+    public static void main(String[]args){
         int i = 0;
         while(i<5){
             System.out.println(i);
